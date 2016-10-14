73c41b966d1cb2c0eac4287599113a5233e9bed6
Kyle Kung <kkung065@uottawa.ca>
2016-10-14 14:05:17 -0400

    Basic backbone of MVC data endpoint

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const influx = require('influx');
+const bodyParser = require('body-parser');
+
+var controller = require('./controllers/main_controller.js');
 
 var app = express();
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
     res.send('Hello World');
 });
 
+app.get('/data/:datasource/:type',controller.getData);
 app.listen(8080);
