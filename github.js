fbacef7f1c1ee113d299dd0776e14935a1831166
BenEmdon <benjaminemdon@gmail.com>
2016-10-14 14:40:15 -0400

    Made changes to github

--- github.js
+++ github.js
@@ -1,23 +1,29 @@
 var request = require('request');
 
 var options = {
-  url: 'https://api.github.com/repos/BenEmdon/Tic-Tac-Toe/commits',
+  url: 'https://api.github.com/repos/BenEmdon/Tic-Tac-Toe/commits', // TODO make dynamic
   headers: {
-    'Authorization': 'token c5ea196043d861110c0d6364105002776ac67105',
-    'user-agent': 'Klipfolio-Decode-2016'
+    'Authorization': 'token c5ea196043d861110c0d6364105002776ac67105', // bens access token
+    'user-agent': 'Klipfolio-Decode-2016-Fall'
   }
 };
 
 function callback(error, response, body) {
-  console.log("entered callback");
-  console.log(JSON.stringify(response));
   if (!error && response.statusCode == 200) {
+    var list = [];
     var info = JSON.parse(body);
-    console.log(JSON.stringify(info, null, '  '));
-    console.log(info[0].commit.author.name);
+
+    for(var i = 0; i < info.length; i++) {
+      list.push({
+        name:info[i].commit.author.name,
+        date:info[i].commit.committer.date
+      })
+    }
+    console.log(JSON.stringify(list, null, '  '));
+  } else {
+    console.log("Failed: " + error + "\t" + response.statusCode);
   }
 }
 module.exports.makeReq = function() {
-  console.log("entered function");
   request(options, callback);
 }
