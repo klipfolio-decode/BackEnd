50164f097c0a58ac0681ea166e40baa57e78c369
BenEmdon <benjaminemdon@gmail.com>
2016-10-15 16:04:55 -0400

    Added new measurement commit length

--- schema.js
+++ schema.js
@@ -1,6 +1,16 @@
 module.exports.datasources = {
   github: {
     measurements: {
+      commitLength: {
+        filter: {
+          required: [
+            'repo'
+          ],
+          optional: [
+            'author'
+          ]
+        }
+      }
       commit: {
         filter: {
           required: [
